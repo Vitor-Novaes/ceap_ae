e6dffe64300e9c509b0126ec8d8a4201bee5a117
Vitor Novaes <33674434+Vitor-Novaes@users.noreply.github.com>
2022-07-28 16:02:09 -0300

    refactor: populate headers bind order data

--- lib/populate/quotas_file_source.rb
+++ lib/populate/quotas_file_source.rb
@@ -3,7 +3,7 @@ module Populate
   class QuotasFileSource
     def initialize(file)
       @csv = nil
-      @header = nil
+      @header = {}
       @file = file
     end
 
@@ -14,7 +14,7 @@ module Populate
       ).sheet(0)
 
       @csv.each.with_index do |row, index|
-        @header = row if index == 0
+        header_mapper(row) if index == 0
         @row = row
 
         if ceara?
@@ -35,67 +35,86 @@ module Populate
     private
 
     def cell(key)
-      map = {
-        deputy_name: @header.find_index('txNomeParlamentar'),
-        deputy_cpf: @header.find_index('cpf'),
-        deputy_ide: @header.find_index('ideCadastro'),
-        deputy_parlamentary_card: @header.find_index('nuCarteiraParlamentar'),
-        deputy_state: @header.find_index('sgUF'),
-        organization_abbreviation: @header.find_index('sgPartido'),
-        category_name: @header.find_index('txtDescricao'),
-        expenditure_especification: @header.find_index('txtDescricaoEspecificacao'),
-        expenditure_provider: @header.find_index('txtFornecedor'),
-        expenditure_provider_documentation: @header.find_index('txtCNPJCPF'),
-        expenditure_receipt_type: @header.find_index('indTipoDocumento'),
-        expenditure_date: @header.find_index('datEmissao'),
-        expenditure_net_value: @header.find_index('vlrLiquido'),
-        expenditure_period: @header.find_index('numAno'),
-        expenditure_receipt_url: @header.find_index('urlDocumento'),
-      }
+      @row.at(@header.fetch(key))
+    end
 
-      @row.at(map[key.to_sym])
+    def header_mapper(row)
+      row.each_index do |index|
+        case row[index]
+        when /txNomeParlamentar/
+          @header.store(:deputy_name, index)
+        when /cpf/
+          @header.store(:deputy_cpf, index)
+        when /ideCadastro/
+          @header.store(:deputy_ide, index)
+        when /nuCarteiraParlamentar/
+          @header.store(:deputy_parlamentary_card, index)
+        when /sgUF/
+          @header.store(:deputy_state, index)
+        when /sgPartido/
+          @header.store(:organization_abbreviation, index)
+        when /txtDescricaoEspecificacao/
+          @header.store(:expenditure_especification, index)
+        when /txtDescricao/
+          @header.store(:category_name, index)
+        when /txtFornecedor/
+          @header.store(:expenditure_provider, index)
+        when /txtCNPJCPF/
+          @header.store(:expenditure_provider_documentation, index)
+        when /indTipoDocumento/
+          @header.store(:expenditure_receipt_type, index)
+        when /datEmissao/
+          @header.store(:expenditure_date, index)
+        when /vlrLiquido/
+          @header.store(:expenditure_net_value, index)
+        when /numAno/
+          @header.store(:expenditure_period, index)
+        when /urlDocumento/
+          @header.store(:expenditure_receipt_url, index)
+        end
+      end
     end
 
     def ceara?
-      cell('deputy_state').match(/CE/)
+      cell(:deputy_state).match(/CE/)
     end
 
     def deputy_name_is_valid?
-      !cell('deputy_name').empty? || !cell('deputy_name').match(/LIDERANÇA/) || !cell('deputy_name').match(/LIDMIN/)
+      !cell(:deputy_name).empty? || !cell(:deputy_name).match(/LIDERANÇA/) || !cell(:deputy_name).match(/LIDMIN/)
     end
 
     def check_deputy_data(organization)
       if deputy_name_is_valid?
-        Deputy.where(cpf: cell('deputy_cpf')).first_or_create(
-          cpf: cell('deputy_cpf'),
-          ide: cell('deputy_ide'),
-          parlamentary_card: cell('deputy_parlamentary_card'),
-          name: cell('deputy_name'),
-          state: cell('deputy_state'),
+        Deputy.where(cpf: cell(:deputy_cpf)).first_or_create(
+          cpf: cell(:deputy_cpf),
+          ide: cell(:deputy_ide),
+          parlamentary_card: cell(:deputy_parlamentary_card),
+          name: cell(:deputy_name),
+          state: cell(:deputy_state),
           organization: organization
         )
       end
     end
 
     def create_expenditure(deputy)
-      category = Category.where(name: cell('category_name')).first_or_create(name: cell('category_name'))
+      category = Category.where(name: cell(:category_name)).first_or_create(name: cell(:category_name))
 
       check_expenditure.first_or_create!(
-        especification: cell('expenditure_especification'),
-        provider: cell('expenditure_provider'),
-        provider_documentation: cell('expenditure_provider_documentation'),
+        especification: cell(:expenditure_especification),
+        provider: cell(:expenditure_provider),
+        provider_documentation: cell(:expenditure_provider_documentation),
         receipt_type: define_receipt_type,
-        date: cell('expenditure_date'),
+        date: cell(:expenditure_date),
         net_value: net_value_definition,
-        period: cell('expenditure_period'),
-        receipt_url: cell('expenditure_receipt_url'),
+        period: cell(:expenditure_period),
+        receipt_url: cell(:expenditure_receipt_url),
         deputy: deputy,
         category: category
       )
     end
 
     def define_receipt_type
-      receipt = cell('expenditure_receipt_type').to_i
+      receipt = cell(:expenditure_receipt_type).to_i
       if receipt.between?(0,4)
         return receipt
       end
@@ -104,8 +123,8 @@ module Populate
     end
 
     def define_date_expenditure
-      unless cell('expenditure_date').nil? || cell('expenditure_date').empty?
-        date = Time.parse(cell('expenditure_date'))
+      unless cell(:expenditure_date).nil? || cell(:expenditure_date).empty?
+        date = Time.parse(cell(:expenditure_date))
         return Time.new(date.year, date.month, date.day, date.hour, date.min, date.sec, "-03:00")
       end
 
@@ -113,28 +132,28 @@ module Populate
     end
 
     def check_belongs_organization
-      if cell('organization_abbreviation').empty?
+      if cell(:organization_abbreviation).empty?
         Organization.where(abbreviation: 'SEM PARTIDO').first_or_create(abbreviation: 'SEM PARTIDO')
       else
         Organization
-          .where(abbreviation: cell('organization_abbreviation'))
-          .first_or_create(abbreviation: cell('organization_abbreviation'))
+          .where(abbreviation: cell(:organization_abbreviation))
+          .first_or_create(abbreviation: cell(:organization_abbreviation))
       end
     end
 
     def net_value_definition
-      return (cell('expenditure_net_value').to_f.ceil(2) * -1) if cell('expenditure_net_value').to_f.negative?
+      return (cell(:expenditure_net_value).to_f.ceil(2) * -1) if cell(:expenditure_net_value).to_f.negative?
 
-      cell('expenditure_net_value').to_f.ceil(2)
+      cell(:expenditure_net_value).to_f.ceil(2)
     end
 
     def check_expenditure
       Expenditure.where(
-        especification: cell('expenditure_especification'),
-        provider: cell('expenditure_provider'),
+        especification: cell(:expenditure_especification),
+        provider: cell(:expenditure_provider),
         date: define_date_expenditure,
         net_value: net_value_definition,
-        receipt_url: cell('expenditure_receipt_url')
+        receipt_url: cell(:expenditure_receipt_url)
       )
     end
   end
